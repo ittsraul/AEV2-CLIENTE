cbf68951e3ab82f802259feeb49852c56887116c
[InquisidorDeLapinya Sergio] <msegio2003@gmail.com>
2022-11-03 13:34:20 +0100

    Logitud

---
+++ sergio.js
@@ -0,0 +1,33 @@
+/* 
+Se almacenan todas las deficinones de cada palabra pasadas por get.
+
+Que el numero de casillas sea igual al numero de caracteres de cada palabra
+
+Por la pantalla se mostraran las letras de las palabras desordenadas.
+
+Crear una tabla debajo de la actual de un color diferente
+
+En cada fila se mostrcada una de las definiciones de las palabras.
+*/
+
+let palabras = ["Software", "Developer", "Engineer", "Ratio", "Flash"];
+
+let definiciones = ["Encoded computer instructions, usually modifiable (unless stored in some form of unalterable memory such as ROM)", "A real estate developer; a person or company who prepares a parcel of land for sale, or creates structures on that land.", "ingeniero", "Ratio", "Flash"];
+
+
+LogitudaPal(palabras[0])
+
+Desordenada(palabras[0])
+
+function LogitudaPal(palabra) {
+    
+  
+    return(palabra.length)
+
+}
+
+function Desordenada(palabra) {
+ 
+
+}
+
