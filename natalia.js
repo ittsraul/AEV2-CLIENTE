b7651668267cd1abc32230cac6167830750bd3ec
Natall1122 <nallca@floridauniversitaria.es>
2022-11-03 13:44:29 +0100

    changesNataliaJS

--- natalia.js
+++ natalia.js
@@ -1,32 +1,29 @@
-/* El teclado permitirá la introducción de palabras. Para ello se implementará el 
-s y cuando 
-finalmente se pulse la tecla ENTER, éstas se guardarán en el navegador, de 
-forma que los datos persistan cuando se cierren las pestañas o se refresque la 
-página. Nota: la tecla ‘DEL’ no es necesario implementarla. */
 
 // Variables    
 let teclado = document.getElementById("keyboard-cont");
-let boton = document.getElementById("boton");
 let letra = "";
 let palabra = "";
-let letras = "";
+let id = 0;
 
 // Eventos
 /* evento de ratón click sobre las letras del teclado de forma que se irán almacenando en una variable cada una de las letras pulsadas */
 teclado.addEventListener("click", function (e) {
     letra = e.target.textContent;
-    letras += letra;
-    console.log(letras);
+    palabra += letra;
+    console.log(palabra);
 });
 
 
-/* evento de teclado para que cuando se pulse la tecla ENTER se guarde la palabra en el navegador */
-let enter = document.getElementsByTagName("button")[28];
+/* evento de teclado para que cuando se pulse la tecla ENTER se guarde la palabra en el navegador, de forma que los datos persistan cuando se cierren las pestañas o se refresque la 
+página */
+let enter = document.getElementsByTagName("form")[0];
 
-enter.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log("hola");
+enter.addEventListener("submit", function () {
+    id++;
+    window.localStorage.setItem(id, JSON.stringify(palabra));
+    
 });
+
 
 
 
