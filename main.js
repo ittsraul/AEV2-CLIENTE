7c77a476ad6fa2c15329a93e13488ffe4ba2f482
Natall1122 <nallca@floridauniversitaria.es>
2022-11-04 09:24:41 +0100

    CanvisMain

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function checkDWord() {
 
 /* NATÀLIA */
 // Variables    
-let teclado = document.getElementById("keyboard-cont");
+/* let teclado = document.getElementById("keyboard-cont"); */
 let letra = "";
 let palabra = "";
 let id = 0;
@@ -84,7 +84,7 @@ let data = [];
 
 // Eventos
 /* evento de ratón click sobre las letras del teclado de forma que se irán almacenando en una variable cada una de las letras pulsadas */
-teclado.addEventListener("click", crearParaula ) 
+keyboard.addEventListener("click", crearParaula ) 
 
 function crearParaula(event) {
     letra = event.target.textContent;
@@ -97,8 +97,9 @@ function crearParaula(event) {
 
 
 /* evento de teclado para que cuando se pulse la tecla ENTER se guarde la palabra en el navegador, de forma que los datos persistan cuando se cierren las pestañas o se refresque la página */
-let enter = document.getElementsByTagName("form")[0];
-enter.addEventListener("submit", function (e) {
+/* enter = document.getElementsByTagName("form")[0]; */
+
+keyboard.addEventListener("submit", function (e) {
     e.preventDefault();
     window.localStorage.setItem(id, JSON.stringify(palabra));
     id++;
