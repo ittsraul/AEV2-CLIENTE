3b44e29e784a4b5735217958a13425d724d70c96
[InquisidorDeLapinya Sergio] <msegio2003@gmail.com>
2022-11-05 17:37:13 +0100

    peque

--- sergio.js
+++ sergio.js
@@ -1,23 +1,13 @@
-/* 
-Se almacenan todas las deficinones de cada palabra pasadas por get.
-
-Que el numero de casillas sea igual al numero de caracteres de cada palabra
-
-Por la pantalla se mostraran las letras de las palabras desordenadas.
-
-Crear una tabla debajo de la actual de un color diferente
-
-En cada fila se mostrcada una de las definiciones de las palabras.
-*/
-
 let palabras = ["Software", "Developer", "Engineer", "Ratio", "Flash"];
 
-let definiciones = ["Encoded computer instructions, usually modifiable (unless stored in some form of unalterable memory such as ROM)", "A real estate developer; a person or company who prepares a parcel of land for sale, or creates structures on that land.", "ingeniero", "Ratio", "Flash"];
+let definiciones = [["Encoded computer instructions, usually modifiable (unless stored in some form of unalterable memory such as ROM)", "una patata"], ["A real estate developer; a person or company who prepares a parcel of land for sale, or creates structures on that land."], ["ingeniero"], ["Ratio"], ["Flash"]];
 
 
 LogitudaPal(palabras[0])
 
 Desordenada(palabras[0])
+
+PasaPalabra(palabras)
 
 function LogitudaPal(palabra) {
     
@@ -29,6 +19,51 @@ function LogitudaPal(palabra) {
 function Desordenada(palabra) {
     let pala=palabra.toLowerCase()
     let shuffled = pala.split('').sort(function(){return 0.5-Math.random()}).join('');
-    console.log(shuffled)
+    return(shuffled);
+    
+
 }
 
+
+
+/* Flujo del juego: cada vez que el usuario haga click sobre el título de primer
+nivel “Let’s Play!”, se mostrará por pantalla una nueva palabra desordenada
+con sus definiciones. El flujo del juego se detendrá al llegar a la última de las
+palabras guardadas en el navegador. */
+
+
+/* 
+1 Hacer que el letsplay sea clicable
+
+2 Obtener una palabra desordenada
+
+3 Dar la definicion correspondiente
+
+4 Al llegar a la ultima mostrar que a terminado
+*/
+
+
+function PasaPalabra(palabra) {
+    let current = 0;
+    let h1 = document.getElementsByTagName("h1")[0];
+  
+
+    h1.addEventListener("click", function(){
+        if(current == palabra.length - 1){
+            current = 0;
+        } else {
+            current += 1;
+        }
+
+        
+     
+    
+       console.log(palabra[current]);
+       
+
+    });
+    
+    
+    
+
+}
